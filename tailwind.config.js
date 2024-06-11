5541295ec008f1aaa7206c4d42e7f0de88796088
chanhee won <wch2208@gmail.com>
2024-06-11 19:11:54 +0900

    refactor: tailwind config, dateUtils 수정
    - tailwind px 단위 적용 대상에 minWidth 등 불필요한 대상 제거
    - dateUtils에서 기준 시간을 밀리세컨드로 만드는 피드백 반영

--- tailwind.config.js
+++ tailwind.config.js
@@ -16,9 +16,6 @@ module.exports = {
       },
       borderWidth: px0_10,
       fontSize: px0_100,
-      lineHeight: px0_100,
-      minWidth: px0_200,
-      minHeight: px0_200,
       spacing: px0_200,
       colors: {
         divider: "#DFDFDF",
